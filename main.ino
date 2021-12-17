ac92194ddace2ef63f2f0ba2a70f06162489d99a
uthman1 <uthman@e-shore.com.my>
2021-12-18 02:46:36 +0800

    Add sending and receiving. Enable receiving only

--- main.ino
+++ main.ino
@@ -1,7 +1,49 @@
-void setup(){
+#include <ArduinoRS485.h>
 
+RS485Class rs485(Serial1, 18, 7, 6);
+
+unsigned long prevMillis;
+int iteration = 0;
+void setup()
+{
+    Serial.begin(115200);
+    rs485.begin(115200);
+    
+    rs485.receive();
 }
 
-void loop(){
+void loop()
+{
+    // reSendingText();
+    receiveText();
+}
 
+void reSendingText(){
+unsigned long now = millis();
+    if (now - prevMillis > 1000)
+    {
+        sendText(iteration);
+        iteration++;
+        if(iteration == 10)
+            iteration = 0;
+        prevMillis = now;
+    }
+}
+
+void sendText(int number)
+{
+    rs485.noReceive();
+    rs485.beginTransmission();
+    rs485.print("hello ");
+    rs485.println(number);
+    rs485.endTransmission();
+    rs485.receive();
+}
+
+void receiveText()
+{
+    if (rs485.available())
+    {
+        Serial.write(rs485.read());
+    }
 }
